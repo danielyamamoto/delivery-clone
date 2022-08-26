ee3f09136f55c46fb79e343f68802596664d86ab
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2022-08-26 17:53:57 -0500

    sanity settings

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,32 @@
-import { React, useLayoutEffect } from 'react';
+import { React, useLayoutEffect, useState, useEffect } from 'react';
 import { View,Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { ChevronDownIcon, UserIcon, SearchIcon, CogIcon } from 'react-native-heroicons/outline';
 import Categories from '../components/Categories';
 import FeaturedRow from '../components/FeaturedRow';
+import SanityClient from '../sanity';
 
 export default function HomeScreen() {
     const navigation = useNavigation();
+    const [featuredCategories, setFeaturedCategories] = useState([]);
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false,
+        });
+    }, [])
+
+    useEffect(() => {
+        SanityClient.fetch(`
+            *[_type == 'featured'] {
+                ...,
+                restaurants[]-> {
+                    ...,
+                    dishes[]->
+                }
+            }
+        `).then((data) => {
+            setFeaturedCategories(data);
         });
     }, [])
 
